f9f86e4af84d27c914a37ed8fed7f986702b6651
Alec Brownlie <alecbrownlie@gmail.com>
2019-03-30 22:51:27 -0700

    more clean up

--- src/com/algorithms/Karatsuba.java
+++ src/com/algorithms/Karatsuba.java
@@ -17,7 +17,8 @@ public class Karatsuba {
 	public String exponentiation(Integer a, Integer b) {
 		if (b == 0) return "1";
 		String val = exponentiation(a, b / 2);
-		return (b % 2 == 0) ? karatsuba_mult(val, val) : karatsuba_mult(karatsuba_mult(val, val), a.toString());
+		String result = karatsuba_mult(val, val);
+		return (b % 2 == 0) ? result : karatsuba_mult(result, a.toString());
 	}
 
 	public String karatsuba_mult(String a, String b) {
@@ -25,10 +26,8 @@ public class Karatsuba {
 		int lengthB = b.length();
 		
 		// base case		
-		if (lengthA == 1 && lengthB == 1) { 
-			return multiply(a, b);
-		}       
-
+		if (lengthA == 1 && lengthB == 1) return multiply(a, b);
+		      
 		// pad zeros to match length
 		if (lengthA < lengthB) a = padZeros(a, b);
 		else if (lengthA > lengthB) b = padZeros(b, a);
@@ -39,11 +38,9 @@ public class Karatsuba {
 		String a1 = a.substring(size / 2, size); 
 		String b0 = b.substring(0, size / 2); 
 		String b1 = b.substring(size / 2, size);
-
 		String c0 = karatsuba_mult(a0, b0);      
 		String c2 = karatsuba_mult(a1, b1);
 		String productABHalves = karatsuba_mult(add(a0, a1), add(b0, b1));
-	
 		String c1  = subtract(subtract(productABHalves, c0), c2);
 		
 		double n = (double) size;
@@ -51,6 +48,7 @@ public class Karatsuba {
 			if (i < Math.ceil(n / 2)) c1 += "0";
 			c0 += "0";
 		}
+		
 		return trimZeros(add(add(c0, c1), c2));
 	}
 }
